535d553208f3dae883c45483b5d20c7c8a505d2c
ciglesiasgastaldi <ciglesiasgastaldi@gmail.com>
2023-02-17 15:00:36 -0300

    my first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var color = [, "#49fff0", "#ffd8f5", 
+        "#969696", "#fdff8a", "#ffcc8a"];
+  
+    document.querySelector("section").addEventListener(
+            "mouseover", function () {
+                  
+        document.querySelector("section").style.background 
+            = color[Math.floor(Math.random() * color.length)];
+
+            
+    })
+
+    
